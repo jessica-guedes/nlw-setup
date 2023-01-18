433b4077c7e70d46b9cacd360c3043394ed00d6a
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-01-18 15:59:55 -0400

    alteração no arquivo seed de T03 para T04

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -38,7 +38,10 @@ export async function appRoutes(app: FastifyInstance){
 
       const { date } = getDayParams.parse(request.query)
 
-      const weekDay = dayjs(date).get('day')
+      const parsedDate = dayjs(date).startOf('day')
+      const weekDay = parsedDate.get('day')
+
+      
 
       const possibleHabits = await prisma.habit.findMany({
         where:{
@@ -53,8 +56,22 @@ export async function appRoutes(app: FastifyInstance){
         }
       })
 
+      const day = await prisma.day.findUnique({
+        where:{
+          date: parsedDate.toDate(),
+        },
+        include:{
+          dayHabits: true
+        }
+      })
+      console.log(parsedDate.toDate())
+      
+      const completedHabits = day?.dayHabits.map(dayHabit => {
+        return dayHabit.habit_id
+      })
       return {
         possibleHabits,
+        completedHabits,
       }
     })
 
