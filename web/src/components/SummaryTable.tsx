1e9683bfbccd792363b6cb0998cb4392bf2de3bc
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-01-18 18:07:08 -0400

    adding the summarydates in the summarytable

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -13,6 +13,9 @@ const weekDays =[
 
 const summaryDates = generateDatesFromYearBeginning()
 
+const minimumSummaryDatesSize = 18 * 7 //18 weeks
+const amoutOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
 export function SummaryTable() {
   return(
     <div className="w-full flex">
@@ -30,38 +33,19 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
+        {summaryDates.map(date => {
+          return <HabitDay key={date.toString()}/>
+        })}
       
-      
-      </div>
+        {amoutOfDaysToFill > 0 && Array.from({ length: amoutOfDaysToFill }).map((_, i) => {
+          return (
+            <div 
+              key={i}
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />
+          )
+        })}
+     </div>
     </div>
   )
 }
