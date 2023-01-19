fe566d5a42783dfe94f7570d20de9d0ecf243051
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-01-19 13:00:13 -0400

    created route completedHabits

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -75,6 +75,57 @@ export async function appRoutes(app: FastifyInstance){
       }
     })
 
-  
-}
+    // Completar / não-completar um hábito
+
+    app.patch('/habits/:id/toggle', async (request) =>{
+
+      const toggleHabitParams = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { id } = toggleHabitParams.parse(request.params)
+
+      const today = dayjs().startOf('day').toDate()
+
+      let day = await prisma.day.findUnique({
+        where:{
+          date: today,
+        }
+      })
+
+      if(!day){
+        day = await prisma.day.create({
+          data: {
+            date: today,
+          }
+        })
+      }
+
+      const dayHabit = await prisma.dayHabit.findUnique({
+        where:{
+          day_id_habit_id: {
+            day_id: day.id,
+            habit_id: id,
+          }
+        }
+      })
+      
+      if(dayHabit){
+        //remover marcação de completo
+        await prisma.dayHabit.delete({
+          where:{
+            id: dayHabit.id,
+          }
+        })
+      }else{
+         // Completar o hábito
+        await prisma.dayHabit.create({
+          data:{
+            day_id: day.id,
+            habit_id: id,
+          }
+        })
+      }
+    }) 
+  }
 
