ca1d9a1d376aede62cd7e44c0849455b4f51fa9b
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-01-19 15:48:56 -0400

    customizing ProgressBar

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,30 @@
+import * as Progress from '@radix-ui/react-progress';
+
+interface ProgressBarProps {
+  progress: number;
+}
+
+export function ProgressBar(props: ProgressBarProps){
+  return(
+    /*<div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
+            <div 
+              role='progressbar'
+              aria-label='Progresso de hábitos completados nesse dia'
+              aria-valuenow={75}
+              className='h-3 rounded-xl bg-violet-600'
+              style={{
+                width: `${props.progress}%`
+              }}
+            />
+      </div> */
+  
+    <Progress.Root className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
+      <Progress.Indicator 
+        className='h-3 rounded-xl bg-violet-600'
+        style={{
+          width: `${props.progress}%`
+        }}
+      />
+    </Progress.Root>
+  )
+}
