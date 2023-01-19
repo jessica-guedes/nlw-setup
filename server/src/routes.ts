6626e3d2cf98974fe225340a83caab73343fabd0
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-01-19 13:38:52 -0400

    days summary route created

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -76,7 +76,6 @@ export async function appRoutes(app: FastifyInstance){
     })
 
     // Completar / não-completar um hábito
-
     app.patch('/habits/:id/toggle', async (request) =>{
 
       const toggleHabitParams = z.object({
@@ -127,5 +126,37 @@ export async function appRoutes(app: FastifyInstance){
         })
       }
     }) 
+
+    app.get('/summary', async () => {
+      // Query mais complexa, mais condições, relacionamentos => SQL na mão (RAW)
+      // Prisma ORM: RAW SQL => SQLite
+
+      const summary = await prisma.$queryRaw`
+        SELECT 
+          D.id, 
+          D.date,
+          (
+            SELECT 
+              cast(count(*) as float)
+            FROM day_habits DH
+            WHERE DH.day_id = D.id
+          ) as completed,
+          (
+            SELECT
+              cast(count(*) as float)
+            FROM habit_week_days HWD
+            JOIN habits H
+              ON H.id = HWD.habit_id
+            WHERE
+              HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+              AND H.created_at <= D.date
+            
+          ) as amount
+        FROM days D
+      `
+      
+      return summary
+    })
+
   }
 
