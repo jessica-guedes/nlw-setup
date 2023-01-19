665cc90d80ff706d83902ad66eca54e2d9fc19cd
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-01-18 20:14:31 -0400

    created habit day component

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -22,5 +22,5 @@ export function Header(){
         </Text>
       </TouchableOpacity>
     </View> 
-  )
+  );
 }
--- mobile/src/components/Loading.tsx
+++ mobile/src/components/Loading.tsx
@@ -5,5 +5,5 @@ export function Loading(){
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#09090A'}}>
       <ActivityIndicator color="#7C3AED"/>
     </View>
-  )
+  );
 }
