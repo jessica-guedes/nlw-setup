50d33417ab058ab008f50ac12508ceb4cea2a89c
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-01-20 15:24:21 -0400

    using clxs to use conditional classes

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,5 +1,8 @@
 import { TouchableOpacity, Dimensions, TouchableOpacityProps } from "react-native";
+import clsx from "clsx";
+
 import { generateProgressPercentage } from '../utils/generate-progress-percentage';
+import dayjs from "dayjs";
 
 const WEEK_DAYS = 7;
 const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
@@ -15,9 +18,21 @@ interface Props  extends TouchableOpacityProps {
 
 export function HabitDay({amountOfHabits = 0, amountCompleted = 0, date, ...rest}: Props){
     const amouthAccomplishedPercentage = amountOfHabits > 0 ? generateProgressPercentage(amountOfHabits, amountCompleted) : 0;
+    const today = dayjs().startOf('day').toDate();
+    const isCurrentDay = dayjs(date).isSame(today);
+
     return(
       <TouchableOpacity
-        className="bg-zinc-800 rounded-lg border-2 m-1 border-zinc-700 "
+        className={clsx('rounded-lg border-2 m-1', {
+          ['bg-zinc-800 border-zinc-700'] : amouthAccomplishedPercentage === 0,
+          ['bg-violet-900 border-violet-700']: amouthAccomplishedPercentage > 0 && amouthAccomplishedPercentage < 20,
+          ['bg-violet-800 border-violet-600']: amouthAccomplishedPercentage >= 20 && amouthAccomplishedPercentage < 40,
+          ['bg-violet-700 border-violet-500']: amouthAccomplishedPercentage >= 40 && amouthAccomplishedPercentage < 60,
+          ['bg-violet-600 border-violet-500']: amouthAccomplishedPercentage >= 60 && amouthAccomplishedPercentage < 80,
+          ['bg-violet-500 border-violet-400']: amouthAccomplishedPercentage >= 80,
+          ['border-white border-3'] : isCurrentDay
+        })}
+
         style={{ width: DAY_SIZE, height: DAY_SIZE }}
         activeOpacity={0.7}
         {...rest}
