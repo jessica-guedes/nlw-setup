560231fa322c5f7417a1b0af84145f0be039d530
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-01-20 20:35:37 -0400

    added transitions and focus

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -20,7 +20,7 @@ export function ProgressBar(props: ProgressBarProps){
   
     <Progress.Root className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
       <Progress.Indicator 
-        className='h-3 rounded-xl bg-violet-600'
+        className='h-3 rounded-xl bg-violet-600 transition-all'
         style={{
           width: `${props.progress}%`
         }}
